6d229438bbbd4fc78b19b6ab73c8dde740d0f6de
ewencre <ewen.cre@gmail.com>
2023-08-23 13:58:02 +0200

    commit initial

---
+++ .history/src/Controller/FortuneController_20230823133105.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FortuneController extends AbstractController
+{
+    #[Route('/', name: 'app_homepage')]
+    public function index(Request $request, CategoryRepository $categoryRepository): Response
+    {
+        $searchTerm = $request->query->get('q');
+
+        if ($searchTerm)
+        {
+            $categories = $categoryRepository->findAllOrdered();
+        }
+        else
+        {
+            
+        }
+
+        return $this->render('fortune/homepage.html.twig',[
+            'categories' => $categories
+        ]);
+    }
+
+    #[Route('/category/{id}', name: 'app_category_show')]
+    public function showCategory(Category $category): Response
+    {
+        return $this->render('fortune/showCategory.html.twig',[
+            'category' => $category
+        ]);
+    }
+}
